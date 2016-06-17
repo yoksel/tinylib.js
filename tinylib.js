d44437000e3209524e3bb2d9a848aafc5cf0c6d1
yokselzok <yoksel@yandex.ru>
2016-06-17 14:29:23 +0300

    Make ElemSet return list of elems. Add prepend() and removeClass();

--- tinylib.js
+++ tinylib.js
@@ -15,104 +15,135 @@
         var doc = document;
         var ns = 'http://www.w3.org/2000/svg';
 
-        function ElemSet( elemsList ) {
-            this.elemsList = elemsList;
-
-            if ( elemsList.length === 1 ){
-              this.elem = elemsList[0];
-            }
+        function ElemSet( elem ) {
+          this.elem = elem;
         }
 
         //---------------------------------
 
         tinyLib.get = function( selector ) {
-            var nodeList = doc.querySelectorAll( selector );
-            var elemsList = Array.prototype.slice.call( nodeList );
+          var nodeList = doc.querySelectorAll( selector );
+          var elemsArr = Array.prototype.slice.call( nodeList );
 
-            return new ElemSet( elemsList );
+          var elemsList = elemsArr.map( function( item ) {
+            return new ElemSet( item );
+          });
+
+          if ( elemsList.length === 1 ) {
+            return elemsList[0];
+          }
+
+          return elemsList;
         };
 
         //---------------------------------
 
         tinyLib.create = function ( tagName ) {
-            var elemList = [ doc.createElement( tagName ) ];
-            return new ElemSet( elemList );
+          var elem = doc.createElement( tagName );
+          return new ElemSet( elem );
         };
 
         //---------------------------------
 
         tinyLib.createNS = function( elemName ) {
-            var elemList = [ doc.createElementNS( ns, tagName ) ];
-            return new ElemSet( elemList );
+          var elem = doc.createElementNS( ns, tagName );
+          return new ElemSet( elem );
         };
 
         //------------------------------
         // ElemSet Methods
 
         ElemSet.prototype.addClass = function ( classNames ) {
-            this.elemsList.forEach( function ( item ){
-                if ( typeof classNames === 'string') {
-                    classNames = [classNames];
-                }
+          var elem = this.elem;
 
-                classNames.forEach( function ( className ){
-                    item.classList.add( className );
-                });
-            });
+          if ( typeof classNames === 'string') {
+            classNames = [classNames];
+          }
 
-            return this;
+          classNames.forEach( function ( className ){
+            elem.classList.add( className );
+          });
+
+          return this;
+        };
+
+        //---------------------------------
+
+        ElemSet.prototype.removeClass = function ( classNames ) {
+
+          var elem = this.elem;
+
+          if ( typeof classNames === 'string') {
+              classNames = [classNames];
+          }
+
+          classNames.forEach( function ( className ){
+              elem.classList.remove( className );
+          });
+
+          return this;
         };
 
         //---------------------------------
 
         ElemSet.prototype.append = function( elem ) {
 
-            this.elemsList.forEach( function ( item ) {
-                var elemToAdd = elem;
+          var elemToAdd = elem.elem;
 
-                if ( typeof elem === 'object' ) {
-                    elemToAdd =  elem.elemsList[0];
-                }
-                else if ( typeof elem === 'string' ) {
-                    elemToAdd = tinyLib.create( elem ).elemsList[0];
-                }
+          if ( typeof elem === 'string' ) {
+            elemToAdd = tinyLib.create( elem ).elem;
+          }
 
-                item.appendChild ( elemToAdd );
-            });
+          this.elem.appendChild( elemToAdd );
 
-            return this;
+          return this;
+        };
+
+        //---------------------------------
+
+        ElemSet.prototype.prepend = function( elem ) {
+
+          var elemToAdd = elem.elem;
+
+          if ( typeof elem === 'string' ) {
+            elemToAdd = tinyLib.create( elem ).elem;
+          }
+
+          this.elem.insertBefore( elemToAdd, this.elem.firstChild );
+
+          return this;
         };
 
         //---------------------------------
 
         ElemSet.prototype.attr = function( attrName, attrVal ) {
 
-            var elem = this.elemsList[0];
-            var attrSet = {};
+          var elem = this.elem;
+          var attrSet = {};
 
-            if ( attrVal ) {
-              attrSet[ attrName ] = attrVal;
-            }
-            else if ( typeof attrName === 'object' ) {
-              attrSet = attrName;
-            }
+          if ( attrVal ) {
+            attrSet[ attrName ] = attrVal;
+          }
+          else if ( typeof attrName === 'object' ) {
+            attrSet = attrName;
+          }
 
-            if ( Object.keys(attrSet).length > 0 ) {
-              for ( var key in attrSet ) {
-                elem.setAttribute( key, attrSet[ key ]);
-              }
-              return this;
+          if ( Object.keys(attrSet).length > 0 ) {
+            for ( var key in attrSet ) {
+              elem.setAttribute( key, attrSet[ key ]);
             }
+            return this;
+          }
 
-            var out = elem.getAttribute( attrName );
-            return out;
+          var out = elem.getAttribute( attrName );
+          return out;
 
         };
 
         //---------------------------------
 
         ElemSet.prototype.html = function ( content ) {
-          var elem = this.elemsList[0];
+          var elem = this.elem;
 
           if( content ) {
             elem.innerHTML = content;
@@ -126,18 +157,22 @@
         // Colored console output
 
         var consoleStyles = {
-            'h1': 'font: 2.5em/1 Arial; color: crimson;',
-            'h2': 'font: 2em/1 Arial; color: orangered;',
-            'h3': 'font: 1.6em/1 Arial; color: olivedrab;',
-            'val': 'font: bold 1.3em/1 Arial; color: midnightblue',
-            'warn': 'padding: 0 .3rem; background: crimson; font: 2em/1 Arial; color: white'
+          'h1': 'font: 2.5em/1 Arial; color: crimson;',
+          'h2': 'font: 2em/1 Arial; color: orangered;',
+          'h3': 'font: 1.6em/1 Arial; color: olivedrab;',
+          'val': 'font: bold 1.3em/1 Arial; color: midnightblue',
+          'warn': 'padding: 0 .3rem; background: crimson; font: 2em/1 Arial; color: white'
         };
 
         tinyLib.out = function( msg, style ) {
-            if ( !style || !consoleStyles[ style ] ) {
-                style = 'val';
-            }
-            console.log( '%c' + msg, consoleStyles[ style ] );
+          if ( !style || !consoleStyles[ style ] ) {
+            style = 'val';
+          }
+          console.log( '%c' + msg, consoleStyles[ style ] );
+        };
+
+        tinyLib.dir = function( msg ) {
+          console.dir( msg );
         };
 
         //---------------------------------
